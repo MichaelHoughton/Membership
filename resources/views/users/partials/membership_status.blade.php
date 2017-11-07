92213f6204e3b587caa7af9b4a33d45bc55abade
Michael Houghton <michael@mclowd.com>
2017-11-07 14:07:39 +0000

    minor Dashboard and layout improvements

--- resources/views/users/partials/membership_status.blade.php
+++ resources/views/users/partials/membership_status.blade.php
@@ -2,6 +2,9 @@
 
 @if ($user->isMember())
     You are currently subscribed to our membership.
+    <a href="{{ url('subscribe') }}">
+        Click here to manage your subscription.
+    </a>
 @else
     <p>
         You are currently not subscribed to our membership. <a href="{{ url('subscribe') }}">Click here to sign up for our membership now</a>.
